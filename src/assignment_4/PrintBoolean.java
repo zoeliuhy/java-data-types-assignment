3d12a9d7b23d5baf58818190dda0e69fbb08073f
zoeliuhy <zoe.liu@rabobank.nl>
2022-11-22 16:04:08 +0100

    Complete assignment 4

---
+++ src/assignment_4/PrintBoolean.java
@@ -0,0 +1,12 @@
+package assignment_4;
+
+public class PrintBoolean {
+    public static void main(String[] args) {
+        boolean test = false;
+        System.out.println(test);
+        test = true;
+        System.out.println(test);
+        test = !test;
+        System.out.println(test);
+    }
+}
